b0c24cd7bcbc467d9a676fc456c18e3750d750fd
Martin Alebachew <martinalebachew@gmail.com>
2023-07-07 19:24:43 +0300

    Added ATR section
    - Added ATR section to report top.
    - Implemented Report::addATRScan().

--- TrafficAnalyser/main.py
+++ TrafficAnalyser/main.py
@@ -16,7 +16,9 @@ def intro():
 def main():
     intro()
     report = Report(datetime.now().strftime("%B %d, %Y at %H:%M:%S"), "Charging RavKav", "Martin Alebachew")
-    report.addRecords(parsePackets("/Users/martin/Desktop/Ithaca Captures/Ithaca_charging.pcap"))
+    recordedPackets = parsePackets("/Users/martin/Desktop/Ithaca Captures/Ithaca_charging.pcap")
+    report.addATRScan(recordedPackets[0])
+    report.addRecords(recordedPackets[1:])
     report.save("/Users/martin/Desktop/generated.pdf")
 
 

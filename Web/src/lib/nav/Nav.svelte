3e67a2b39d4b91ffada4cc56b4dd1b3b683f1b48
Martin Alebachew <martinalebachew@gmail.com>
2024-05-17 20:15:23 +0300

    Added navbar

---
+++ Web/src/lib/nav/Nav.svelte
@@ -0,0 +1,67 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/pcap">Load .pcap</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.2);
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 2em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>

de92e98066eacdb851fa1d2216cf3f871d5fc743
Martin Alebachew <martinalebachew@gmail.com>
2024-05-17 20:16:20 +0300

    Added layout and home page

---
+++ Web/src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import '$lib/styles/index.css';
+	import { Nav } from '$lib/nav';
+</script>
+
+<svelte:head>
+	<title>Ithaca Web Portal</title>
+</svelte:head>
+
+<div class="app">
+	<Nav></Nav>
+
+	<main>
+		<slot></slot>
+	</main>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
--- Web/src/routes/+page.svelte
+++ Web/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import RavKavImage from '$lib/images/ravkav-envelope.png';
+</script>
+
+<svelte:head>
+</svelte:head>
+
+<section>
+	<h1>Ithaca Web Portal // RavKav Research</h1>
+	<p>Choose an operation mode from the menu above.</p>
+	
+	<img src={RavKavImage} alt="RavKav" />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>

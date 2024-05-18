bc01e976f52675eb6da9eba1add632b4fdc04b44
Martin Alebachew <martinalebachew@gmail.com>
2024-05-18 13:24:37 +0300

    Implemented Native Messaging output

---
+++ Parser/NativeMessaging.py
@@ -0,0 +1,23 @@
+# NativeMessaging.py
+# (C) Martin Alebachew, 2024
+
+import sys
+import struct
+
+LENGTH_SIZE = 4
+LENGTH_ENDIANNESS = "little"
+
+
+def disableOutputAltering() -> None:
+  # TODO: Make sure Windows does not replace /n with /r/n
+  pass
+
+
+def encodeLength(data: bytes) -> bytes:
+  return struct.pack('@I', len(data))
+
+
+def out(data: bytes) -> None:
+  disableOutputAltering()
+  sys.stdout.buffer.write(encodeLength(data))
+  sys.stdout.buffer.write(data)
